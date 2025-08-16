e6467305f5d20f1c76b22ed27815ac123671dc52
howudong <tjdvy963@naver.com>
2025-08-16 18:45:38 +0900

    Chore : NoArgs 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,12 @@ allOpen {
     annotation("jakarta.persistence.Embeddable")
 }
 
+noArg {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
