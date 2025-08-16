b8139652fe23d395c046e229da42af383ce2725b
howudong <tjdvy963@naver.com>
2025-08-16 20:02:06 +0900

    Feat : AbstractEntity 정의

---
+++ src/main/kotlin/aio/intro/domain/AbstractEntity.kt
@@ -0,0 +1,25 @@
+package aio.intro.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
+
+@MappedSuperclass
+abstract class AbstractEntity protected constructor(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @CreatedDate
+    @Column(updatable = false)
+    val created_at: LocalDateTime,
+
+    ) {
+    @LastModifiedDate
+    var updated_at: LocalDateTime = LocalDateTime.now()
+        protected set
+}
