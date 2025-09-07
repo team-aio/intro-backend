531ac7c3e078a33985a36f206572dbf40d585e84
howudong <tjdvy963@naver.com>
2025-09-07 22:53:47 +0900

    Feat : JSON 변환용 DTO 정의

---
+++ src/main/kotlin/aio/intro/adapter/integration/notion/dto/NumberProperty.kt
@@ -0,0 +1,6 @@
+package aio.intro.adapter.integration.notion.dto
+
+data class NumberProperty(
+    val type: String = "number",
+    val number: Double,
+)
---
+++ src/main/kotlin/aio/intro/adapter/integration/notion/dto/TitleProperty.kt
@@ -0,0 +1,5 @@
+package aio.intro.adapter.integration.notion.dto
+
+data class TitleProperty(val type: String = "title", val title: List<RichText>)
+data class RichText(val type: String = "text", val text: TextContent)
+data class TextContent(val content: String)
